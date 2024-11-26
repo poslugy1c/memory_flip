5f2b14b9616920dcb7321f7ac1e1868ceb28d1d9
webkhmelevska <webkhmelevska@gmail.com>
2024-11-26 22:59:09 +0200

    installed vite, tailwind, added images and main styles

---
+++ memory_flip-app/src/App.jsx
@@ -0,0 +1,24 @@
+import { data } from './data';
+
+
+function App() {
+  return (
+    <section className={'container'}>
+      <button className={'mx-auto mt-10 flex items-center justify-center rounded-2xl bg-green-800 px-10 py-2 text-2xl font-bold text-white'}>
+        Start
+      </button>
+      {
+        data.map((card) => {
+          return (<div key={card.id}> 
+            <img  src={card.imgUrl} alt={card.label} width={60} />
+          </div>) 
+        }
+      )}
+      <h1 className={'text-center text-4xl font-bold '}>Score: 0</h1>
+    </section>
+  )
+}
+
+export default App
+
+//[#098114A]
---
+++ memory_flip-app/src/data.js
@@ -0,0 +1,92 @@
+export const data = [
+  {
+    id: 0,
+    imgUrl: "./logos/react.svg",
+    label: "React.js",
+    matched: false,
+  },
+  {
+    id: 1,
+    imgUrl: "./logos/angular.svg",
+    label: "Angular.js",
+    matched: false,
+  },
+  {
+    id: 2,
+    imgUrl: "./logos/astro.svg",
+    label: "Astro",
+    matched: false,
+  },
+  {
+    id: 3,
+    imgUrl: "./logos/ember.svg",
+    label: "Ember",
+    matched: false,
+  },
+  {
+    id: 4,
+    imgUrl: "./logos/laravel.svg",
+    label: "Laravel",
+    matched: false,
+  },
+  {
+    id: 5,
+    imgUrl: "./logos/node.svg",
+    label: "Node.js",
+    matched: false,
+  },
+  {
+    id: 6,
+    imgUrl: "./logos/nuxt.svg",
+    label: "Nuxt",
+    matched: false,
+  },
+  {
+    id: 7,
+    imgUrl: "./logos/preact.svg",
+    label: "Preact",
+    matched: false,
+  },
+  {
+    id: 8,
+    imgUrl: "./logos/remix.svg",
+    label: "Remix",
+    matched: false,
+  },
+  {
+    id: 9,
+    imgUrl: "./logos/solid.svg",
+    label: "Solid",
+    matched: false,
+  },
+  {
+    id: 10,
+    imgUrl: "./logos/storybook.svg",
+    label: "Storybook",
+    matched: false,
+  },
+  {
+    id: 11,
+    imgUrl: "./logos/svelte.svg",
+    label: "Svelte",
+    matched: false,
+  },
+  {
+    id: 12,
+    imgUrl: "./logos/vite.svg",
+    label: "Vite",
+    matched: false,
+  },
+  {
+    id: 13,
+    imgUrl: "./logos/mongo.svg",
+    label: "Mongo DB",
+    matched: false,
+  },
+  {
+    id: 0,
+    imgUrl: "./logos/vue.svg",
+    label: "Vue.js",
+    matched: false,
+  },
+];
